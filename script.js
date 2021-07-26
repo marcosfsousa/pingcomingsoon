895381026eedc2b6d15ad9dec4a6a3a33eb34f6a
marcosfsousa <marcosfmsousa@gmail.com>
2021-07-26 17:59:15 +0200

    Refactored breakpoints

--- script.js
+++ script.js
@@ -4,25 +4,38 @@ const inputBox = document.querySelector(".input-box");
 const button = document.querySelector(".btn-notify");
 const warning = document.createElement("span");
 
+
+
+
+
 function ValidateForm(event) {
     const emailValue = email.value.trim(); //trim() removes blank characters from the start of the string //
     
     if (emailValue === '') {
-        button.style.margin = "20px 10px 0px";        
+        // button.style.margin = "20px 10px 0px";        
         email.parentElement.insertBefore(warning, inputBox.children[1]); //show error 
         warning.className = ("error"); // adding error class       
         email.className = ("input-email red-border") //adding error class
         warning.textContent = "Whoops! It looks like you forgot to add your email" //add error message      
+
+        if (window.innerWidth >= 768) {
+            warning.className = ("error desktop-error")
+        }
         
     } else if (!emailTest(emailValue)) {        
-        button.style.margin = "20px 10px 0px";      
+        // button.style.margin = "20px 10px 0px";      
         email.parentElement.insertBefore(warning, inputBox.children[1]); //show error 
         warning.className = ("error"); // add error class
         email.className = ("input-email red-border") //adding error class
         warning.textContent = "Please provide a valid email address";
         email.placeholder = "example@email/com"
+        
+        if (window.innerWidth >= 768) {
+            warning.className = ("error desktop-error")
+        }
+
     } else {
-        button.style.margin = "0px 10px 0px";
+        // button.style.margin = "0px 10px 0px";
         email.className = ("input-email green-border");
         inputBox.removeChild(warning);
     }
@@ -33,3 +46,5 @@ function ValidateForm(event) {
 function emailTest (email) {
     return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(email);
 }
+
+
