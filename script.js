0a2442b55dd1d52a10bef35a66cda8e1b33402f5
marcosfsousa <marcosfmsousa@gmail.com>
2022-02-17 16:01:17 +0100

    Refactored logic and styles accordingly

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-
 const email = document.querySelector(".input-email");
 const inputBox = document.querySelector(".input-box");
 const button = document.querySelector(".btn-notify");
 const warning = document.createElement("span");
 
-
 function ValidateForm(event) {
-    const emailValue = email.value.trim(); //trim() removes blank characters from the start of the string //
-    
-    if (emailValue === '') {
-              
-        email.parentElement.insertBefore(warning, inputBox.children[1]); //show error 
-        warning.className = ("error"); // adding error class       
-        email.className = ("input-email red-border") //adding error class
-        warning.textContent = "Whoops! It looks like you forgot to add your email" //add error message      
+  const emailValue = email.value.trim(); //trim() removes blank characters from the start of the string //
 
-        if (window.innerWidth >= 768) {
-            warning.className = ("error desktop-error")
-        }
-        
-    } else if (!emailTest(emailValue)) {        
-        email.parentElement.insertBefore(warning, inputBox.children[1]); //show error 
-        warning.className = ("error"); // add error class
-        email.className = ("input-email red-border") //adding error class
-        warning.textContent = "Please provide a valid email address";
-        email.placeholder = "example@email/com"
-        
-        if (window.innerWidth >= 768) {
-            warning.className = ("error desktop-error")
-        }
+  if (emailValue === "") {
+    email.parentElement.insertBefore(warning, inputBox.children[1]); //show error
+    warning.className = "error"; // adding error class
+    email.className = "input-email red-border"; //adding error class
+    warning.textContent = "Whoops! It looks like you forgot to add your email"; //add error message
 
-    } else {
-        email.className = ("input-email green-border");
-        inputBox.removeChild(warning);
+    if (window.innerWidth >= 768) {
+      warning.className = "error desktop-error";
     }
-        
-    event.preventDefault(); 
+  } else if (!emailTest(emailValue)) {
+    email.parentElement.insertBefore(warning, inputBox.children[1]); //show error
+    warning.className = "error"; // add error class
+    email.className = "input-email red-border"; //adding error class
+    warning.textContent = "Please provide a valid email address";
+    email.placeholder = "example@email/com";
+
+    if (window.innerWidth >= 768) {
+      warning.className = "error desktop-error";
     }
-        
-function emailTest (email) {
-    return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(email);
+  } else {
+    email.parentElement.insertBefore(warning, inputBox.children[1]);
+    warning.className = "accepted";
+    email.className = "input-email green-border";
+    warning.textContent = "Your email is added to the subscriber list.";
+
+    if (window.innerWidth >= 768) {
+      warning.className = "accepted desktop-accepted";
+    }
+  }
+
+  event.preventDefault();
 }
 
-
+function emailTest(email) {
+  return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(
+    email
+  );
+}
